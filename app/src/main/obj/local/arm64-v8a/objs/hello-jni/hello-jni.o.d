5fcb1e29947570e18497786d97f7d654e8ca8e79
HayaoSasaki <myhhc912@yahoo.co.jp>
2020-11-26 22:58:19 +0900

    deleted junk

--- app/src/main/obj/local/arm64-v8a/objs/hello-jni/hello-jni.o.d
+++
@@ -1,2 +0,0 @@
-E:/dev/android/sha256/app/src/main/obj/local/arm64-v8a/objs/hello-jni/hello-jni.o: \
-  E:\dev\android\sha256\app\src\main\jni\hello-jni.c
--- app/src/main/obj/local/x86_64/objs/hello-jni/hello-jni.o.d
+++
@@ -1,2 +0,0 @@
-E:/dev/android/sha256/app/src/main/obj/local/x86_64/objs/hello-jni/hello-jni.o: \
-  E:\dev\android\sha256\app\src\main\jni\hello-jni.c
